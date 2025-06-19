f78e105eefcd49e25c2f4e4a3b5e0fb8304795e8
Anastasiya Zagvozdkina <nastya0204z@gmail.com>
2025-06-19 14:54:51 +0500

    downloaded pydantic email-validator and fixed parse_mode in bot code for the new aiogram version

--- app/bot/instance.py
+++ app/bot/instance.py
@@ -2,6 +2,10 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from app.core.config import settings
+from aiogram.client.default import DefaultBotProperties
 
-bot = Bot(token=settings.TELEGRAM_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=settings.TELEGRAM_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML) #изменение под новую вурсию aiogram
+)
 dp = Dispatcher(storage=MemoryStorage())
